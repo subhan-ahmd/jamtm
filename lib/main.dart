93e92599be2ff3f32cbec37f92c82f201a803fc6
Subhan Ahmed <subhan.ahmd@tuta.io>
2025-07-20 14:55:20 +0500

    refactor: separate app code from basic scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mini_task_manager/screens/calendar_view/calendar_view_screen.dart';
+import 'package:mini_task_manager/screens/home/home_screen.dart';
+import 'package:mini_task_manager/screens/schedule_view/schedule_view_screen.dart';
 import 'package:mini_task_manager/utils/theme_manager.dart';
 import 'package:mini_task_manager/widgets/basic_scaffold/basic_scaffold.dart';
 
@@ -16,7 +19,30 @@ class MiniTaskManager extends StatelessWidget {
       title: 'Mini Task Manager',
       themeMode: ThemeMode.light,
       theme: ThemeManager.appTheme,
-      home: BasicScaffold(),
+      home: BasicScaffold(
+         destinations: const <NavigationDestination>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.task),
+            icon: Icon(Icons.task_outlined),
+            label: 'Tasks',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.calendar_today),
+            icon: Icon(Icons.calendar_today_outlined),
+            label: 'Calendar View',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.calendar_view_day),
+            icon: Icon(Icons.calendar_view_day_outlined),
+            label: 'Schedule View',
+          ),
+        ],
+        screens: <Widget>[
+        HomeScreen(),
+        CalendarViewScreen(),
+        ScheduleViewScreen(),
+      ],
+      ),
     );
   }
 }
--- lib/widgets/basic_scaffold/basic_scaffold.dart
+++ lib/widgets/basic_scaffold/basic_scaffold.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mini_task_manager/screens/calendar_view/calendar_view_screen.dart';
-import 'package:mini_task_manager/screens/schedule_view/schedule_view_screen.dart';
 import 'package:mini_task_manager/widgets/basic_scaffold/providers/current_tab_provider.dart';
-import 'package:mini_task_manager/screens/home/home_screen.dart';
 
 class BasicScaffold extends ConsumerWidget {
-  const BasicScaffold({super.key});
+  final List<NavigationDestination> destinations;
+  final List<Widget> screens;
+  const BasicScaffold({
+    super.key,
+    this.destinations = const [],
+    this.screens = const [],
+  });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -16,31 +19,10 @@ class BasicScaffold extends ConsumerWidget {
         onDestinationSelected: (int index) {
           ref.read(currentTabProvider.notifier).toggle(index);
         },
-        indicatorColor: Colors.amber,
         selectedIndex: currentTab,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(Icons.task),
-            icon: Icon(Icons.task_outlined),
-            label: 'Tasks',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.calendar_today),
-            icon: Icon(Icons.calendar_today_outlined),
-            label: 'Calendar View',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.calendar_view_day),
-            icon: Icon(Icons.calendar_view_day_outlined),
-            label: 'Schedule View',
-          ),
-        ],
+        destinations: destinations,
       ),
-      body: <Widget>[
-        HomeScreen(),
-        CalendarViewScreen(),
-        ScheduleViewScreen(),
-      ][currentTab],
+      body: screens[currentTab],
     );
   }
 }
